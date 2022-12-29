015db8895cdf792c24caccca608894400a88eaa0
Luis Dourado <lfaddourado@gmail.com>
2022-12-29 10:59:47 -0300

    feat: add initialization of the discord bot

--- bot/bot.go
+++ bot/bot.go
@@ -1,1 +1,29 @@
 package bot
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"log"
+	"os"
+	"os/signal"
+)
+
+var BotToken string
+
+func Start() {
+
+	discord, err := discordgo.New("Bot " + BotToken)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	//discord.AddHandler(Message)
+	discord.Open()
+
+	defer discord.Close()
+	fmt.Print("Bot foi iniciado com sucesso")
+
+	c := make(chan os.Signal, 1)
+
+	signal.Notify(c, os.Interrupt)
+	<-c
+}
